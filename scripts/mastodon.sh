6669d41288a073407e0a0cd7cef8929abcd22018
Thunder <mahmmoud.hassanein@gmail.com>
2022-11-13 16:24:55 +0200

    Change create user from command to orm

--- scripts/mastodon.sh
+++ scripts/mastodon.sh
@@ -1,18 +1,29 @@
 #!/bin/bash
+
+script="
+username = '$SUPERUSER_USERNAME'
+password = '$SUPERUSER_PASSWORD'
+email = '$SUPERUSER_EMAIL'
+
+account = Account.create!(username: 'username')
+user = User.create!(email: 'email', password: 'password', account: account)
+user.confirm
+account.save!
+user.save!
+"
+
 cd docker
 chown -R 991:991 public
-if [ -z "$SUPERUSER_USERNAME" ] || [ -z "$SUPERUSER_EMAIL" ]; then
+if [ -z "$SUPERUSER_USERNAME" ] || [ -z "$SUPERUSER_EMAIL" ] || [ -z "$SUPERUSER_PASSWORD" ]; then
         echo "Error: Missing required env vars! Superuser creation skipped."
         exit 1
 else
     # Create and migrate the database.
     docker-compose run --rm web bundle exec rails db:setup
     # Create admin user with given email
-    result=$(RAILS_ENV=production docker-compose run --rm web bin/tootctl accounts create $SUPERUSER_USERNAME --email $SUPERUSER_EMAIL --confirmed --role Owner)
-    # Get the password of created account then save it into .env.production
-    SUPERUSER_PASSWORD=$(echo $result | awk -F: '{print $2}')
-    echo SUPERUSER_PASSWORD=$SUPERUSER_PASSWORD | sed 's/ //g' >> .env.production
+    RAILS_ENV=production docker-compose run --rm web bundle bash -c "printf \"$script\" | exec rails c"
+
     # Up all containers
-    docker-compose up 
+    docker-compose up
 fi
 # rails --tasks
