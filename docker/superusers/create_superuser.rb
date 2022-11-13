e5b877a706cbac44e264c49920a6cfb79ec41588
Thunder <mahmmoud.hassanein@gmail.com>
2022-11-13 20:49:38 +0200

    Change create user from command to orm

--- docker/superusers/create_superuser.rb
+++ docker/superusers/create_superuser.rb
@@ -3,7 +3,12 @@ password = ENV['SUPERUSER_PASSWORD']
 email = ENV['SUPERUSER_EMAIL']
 
 account = Account.create!(username: username)
-user = User.create!(email: email, password: password, account: account)
+user = User.create!(
+    email: email,
+    password: password,
+    account: account,
+    agreement: true
+)
 user.confirm
 account.save!
 user.save!
--- scripts/mastodon.sh
+++ scripts/mastodon.sh
@@ -9,8 +9,7 @@ else
     # Create and migrate the database.
     docker-compose run --rm web bundle exec rails db:setup
     # Create admin user with given email
-    SUPERUSER_USERNAME=$SUPERUSER_USERNAME SUPERUSER_EMAIL=$SUPERUSER_EMAIL \
-    SUPERUSER_PASSWORD=$SUPERUSER_PASSWORD RAILS_ENV=production docker-compose run --rm web ruby superusers/create_superuser.rb
+    RAILS_ENV=production docker-compose run --rm web bundle exec rails r superusers/create_superuser.rb 
     # Up all containers
     docker-compose up
 fi
