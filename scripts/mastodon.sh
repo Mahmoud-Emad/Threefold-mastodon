a102f5d622c0ad892f29a026a76b261ca185cfaf
Thunder <mahmmoud.hassanein@gmail.com>
2022-11-13 13:55:59 +0200

    Create admin account and save its password into .env file

--- scripts/mastodon.sh
+++ scripts/mastodon.sh
@@ -5,7 +5,14 @@ if [ -z "$SUPERUSER_USERNAME" ] || [ -z "$SUPERUSER_EMAIL" ]; then
         echo "Error: Missing required env vars! Superuser creation skipped."
         exit 1
 else
+    # Create and migrate the database.
     docker-compose run --rm web bundle exec rails db:setup
-    docker-compose run --rm web bundle exec rails mastodon:make_admin USERNAME=$SUPERUSER_USERNAME EMAIL=$SUPERUSER_EMAIL
+    # Create admin user with given email
+    result=$(docker-compose run --rm web RAILS_ENV=production bin/tootctl accounts create $SUPERUSER_USERNAME --email $SUPERUSER_EMAIL --confirmed --role Owner)
+    # Get the password of created account then save it into .env.production
+    SUPERUSER_PASSWORD=$(echo $result | awk -F: '{print $2}')
+    echo SUPERUSER_PASSWORD=$SUPERUSER_PASSWORD >> .env.production
+    # Up all containers
     docker-compose up 
 fi
+# rails --tasks
